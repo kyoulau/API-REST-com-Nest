cac153b7d98b662cd12f6994d3f1df4ca4d077fe
kyoulau <laucosan05@gmail.com>
2025-02-21 15:02:58 -0300

    new module for user

---
+++ src/user/DTO/update-user-dto.ts
@@ -0,0 +1,23 @@
+import { PartialType } from "@nestjs/mapped-types";
+import { CreateUserDto } from "./create-user-dto";
+import { IsOptional } from "class-validator";
+
+export class UpdateUserDto{
+
+  @IsOptional()
+  username: string
+
+  @IsOptional()
+  userEmail: string
+
+  @IsOptional()
+  userPassword: string
+
+  @IsOptional()
+  userRoleAtributed: string
+
+  @IsOptional()
+  active: Boolean
+
+
+}
